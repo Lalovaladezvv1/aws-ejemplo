4a274fce9332da48ff959484a683f24a32b36e8f
Lalovaladezvv1 <lalovaladezvv1@gmail.com>
2021-07-21 02:07:54 -0500

    Texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Word with express'))
+app.get('/', (req, res) => res.send('<h1>Hello Word with express and html</h1>'))
 
 app.listen(3000);
 console.log('server on port 3000');
